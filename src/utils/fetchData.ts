3d2239d574f524c3640135cd2fb44bea3dc96523
Igor M. <samplefines@gmail.com>
2025-07-31 16:29:16 +0300

    error handling

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -5,14 +5,21 @@ export async function fetchData(
   searchPrompt: string,
   type: string
 ) {
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchPrompt}${
-      type ? `&type=${type}` : ''
-    }`
-  );
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchPrompt}${
+        type ? `&type=${type}` : ''
+      }`
+    );
 
-  const data: ResponseOmdbApi = await response.json();
+    const data: ResponseOmdbApi = await response.json();
 
-  //   console.log(data);
-  return data;
+    //   console.log(data);
+    return data;
+  } catch (error) {
+    return {
+      Response: false,
+      Error: 'Error: ' + (error as Error).message,
+    };
+  }
 }
